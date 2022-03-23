600ea2704ff4dff27af5b3269e1f486a8849bedd
Alejandro Ramirez Aldariz <627538+janodev@users.noreply.github.com>
2022-03-23 10:58:35 +0100

    Removes macOS compatibility

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Dependency",
     platforms: [
-        .iOS(.v15), .macOS(.v12)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "Dependency", type: .dynamic, targets: ["Dependency"]),
